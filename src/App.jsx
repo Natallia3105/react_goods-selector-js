2856739e88625d0b9876ab05fba9e4fc9b019766
Nataliia Honcharuk <133349158+Natallia3105@users.noreply.github.com>
2024-06-12 14:31:08 +0200

    Update src/App.jsx
    Co-authored-by: anastasiiavorobiova <135232001+anastasiiavorobiova@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ export const App = () => {
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={() => setSelectedGood('')}
+            onClick={reset}
           />
         )}
       </h1>
